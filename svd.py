c6ed9a2c7bea6c84db425c977df25dc397a9cd3a
Wayne Small <waynemystir@gmail.com>
2018-01-02 15:44:58 -0500

    fix svd for case where more rows than columns in given matrix

--- svd.py
+++ svd.py
@@ -8,19 +8,19 @@ from math import sqrt
 def randomUnitVector(n):
     unnormalized = [normalvariate(0, 1) for _ in range(n)]
     theNorm = sqrt(sum(x * x for x in unnormalized))
-    return [x / theNorm for x in unnormalized]
+    return np.array([x / theNorm for x in unnormalized])
 
 
 def svd_1d(A, epsilon=1e-10):
     ''' The one-dimensional SVD '''
 
     if type(A) == list: A=np.array(A, dtype=float)
-    n, m = A.shape
-    x = randomUnitVector(m)
+    m, n = A.shape
+    x = randomUnitVector(min(m,n))
     lastV = None
     currentV = x
 
-    if n > m:
+    if m >= n:
         B = np.dot(A.T, A)
     else:
         B = np.dot(A, A.T)
@@ -36,21 +36,29 @@ def svd_1d(A, epsilon=1e-10):
             print("converged in {} iterations!".format(iterations))
             return currentV
 
-def svd(A, epsilon=1e-10):
-    if type(A) == list: A=np.array(A, dtype=float)
-    n, m = A.shape
+def svd(A, k=None, epsilon=1e-10):
+    A=np.array(A, dtype=float)
+    m, n = A.shape
     svdSoFar = []
+    if k is None:
+        k = min(m,n)
  
-    for i in range(m):
+    for i in range(k):
         matrixFor1D = A.copy()
  
         for singularValue, u, v in svdSoFar[:i]:
             matrixFor1D -= singularValue * np.outer(u, v)
  
-        v = svd_1d(matrixFor1D, epsilon=epsilon)  # next singular vector
-        u_unnormalized = np.dot(A, v)
-        sigma = norm(u_unnormalized)  # next singular value
-        u = u_unnormalized / sigma
+        if m >= n:
+            v = svd_1d(matrixFor1D, epsilon=epsilon)  # next singular vector
+            u_unnormalized = np.dot(A, v)
+            sigma = norm(u_unnormalized)  # next singular value
+            u = u_unnormalized / sigma
+        else:
+            u = svd_1d(matrixFor1D, epsilon=epsilon)  # next singular vector
+            v_unnormalized = np.dot(A.T, u)
+            sigma = norm(v_unnormalized)  # next singular value
+            v = v_unnormalized / sigma
  
         svdSoFar.append((sigma, u, v))
  
